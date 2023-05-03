3685858ffc462265c6ccc65740c523b030a12f99
rahmanhas <rahmanhas@gmail.com>
2023-05-03 13:18:37 +0600

    environment variable updated

--- firebase.config.js
+++ firebase.config.js
@@ -8,12 +8,12 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBbteHY-M30wFhNSW5JwsKX4zcBR278yZM",
-  authDomain: "chef-recipe-hunter-rahmanhas.firebaseapp.com",
-  projectId: "chef-recipe-hunter-rahmanhas",
-  storageBucket: "chef-recipe-hunter-rahmanhas.appspot.com",
-  messagingSenderId: "1094563010316",
-  appId: "1:1094563010316:web:9de584a4916aae9d0c9b29"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
